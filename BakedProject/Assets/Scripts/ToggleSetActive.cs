b474d4b60345297fbb8ddf95683c0bc4c35f0448
VocaloidZero <341151@colum.edu>
2020-03-14 22:36:38 -0400

    Toggle script
    Toggle will only work when placed on the already non interactive items...
    
    The objects with the "Interactive Object" script will not toggle and that's an issue.

---
+++ BakedProject/Assets/Scripts/ToggleSetActive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSetActive : InteractiveObject
+{
+    [Tooltip("The game object to toggle.")]
+    [SerializeField]
+    private GameObject objectToToggle;
+    private GameObject droppedItem;
+
+    [Tooltip("Can the player interact with this more than once?")]
+    [SerializeField]
+    private bool isReuseable = true;
+
+    private bool hasBeenUsed = false;
+
+    /// <summary>
+    /// Toggles the activeSelf value for the objectToToggle when the player interacts with this item.
+    /// </summary>
+    public override void InteractWith()
+    {
+        if(isReuseable || !hasBeenUsed)
+        {
+            Debug.Log("Toggled");
+            base.InteractWith();
+            objectToToggle.SetActive(!objectToToggle.activeSelf);
+           // droppedItem.SetActive(true);
+            hasBeenUsed = true;
+            if (!isReuseable)  displayText = string.Empty;
+        }
+        
+    }
+
+}
