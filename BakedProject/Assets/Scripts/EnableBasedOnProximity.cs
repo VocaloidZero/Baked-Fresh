d2ada0fcf06948640cff254e2473d702c10c566d
VocaloidZero <341151@colum.edu>
2020-05-14 19:54:12 -0400

    Fadeout Title Transition

---
+++ BakedProject/Assets/Scripts/EnableBasedOnProximity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableBasedOnProximity : MonoBehaviour
+{
+    public GameObject loading;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            loading.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            loading.SetActive(false);
+
+        }
+    }
+
+
+
+}
