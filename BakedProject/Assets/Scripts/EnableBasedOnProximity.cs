ee5557a5f27b837580bd8fb93fd04832884cf899
VocaloidZero <341151@colum.edu>
2020-05-14 23:04:22 -0400

    Fixed Transitions
    fixed top floor ground pattern
    Cake wobble

--- BakedProject/Assets/Scripts/EnableBasedOnProximity.cs
+++ BakedProject/Assets/Scripts/EnableBasedOnProximity.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class EnableBasedOnProximity : MonoBehaviour
 {
-    public GameObject loading;
+    [SerializeField]
+    private GameObject loading;
 
     private void OnTriggerEnter(Collider other)
     {
