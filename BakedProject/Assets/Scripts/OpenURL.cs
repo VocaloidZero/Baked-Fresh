85770299c590521b4f8d45aed31938fc7a2bd780
VocaloidZero <341151@colum.edu>
2020-05-15 07:05:58 -0400

    Intro scene addition
    Steam added
    Buttons in credits
    compile build

---
+++ BakedProject/Assets/Scripts/OpenURL.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    [SerializeField]
+    private string UrlOpener;
+
+    void Start()
+    {
+        
+    }
+
+   public void Open()
+    {
+        Application.OpenURL(UrlOpener);
+    }
+}
